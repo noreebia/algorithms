f152a856d6fc51a931df5bd1277a94d54c5c39d4
noreebia <noreebia@gmail.com>
2018-09-27 19:43:17 +0900

    Add project

---
+++ src/nhn_entertainment/ItemAdventure.java
@@ -0,0 +1,80 @@
+package nhn_entertainment;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class ItemAdventure {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String itemString = scanner.nextLine();
+		scanner.close();
+
+		if(itemString.length() == 0 || itemString == null){
+			System.out.println(0);
+			return;
+		}
+		
+		String[] stringsOfItems = itemString.split("\\s+");
+
+		int numOfInputs = stringsOfItems.length;
+		int[] itemsPickedUp = new int[numOfInputs];
+		for (int i = 0; i < numOfInputs; i++) {
+			itemsPickedUp[i] = Integer.parseInt(stringsOfItems[i]);
+//			System.out.print(itemsPickedUp[i] + " ");
+		}
+		
+//		System.out.println();
+
+		LinkedList<Integer> playerItems = new LinkedList<>();
+		List<Integer> droppedItems = new ArrayList<>();
+		
+		
+		for (Integer item : itemsPickedUp) {
+			if (!playerItems.contains(item)) {
+				if (playerItems.size() < 3) {
+					playerItems.addFirst(item);
+				} else {
+//					droppedItems.add(playerItems.get(playerItems.size()-1));
+//					playerItems.remove(playerItems.size() - 1);
+					
+					droppedItems.add(playerItems.removeLast());
+					playerItems.addFirst(item);
+				}
+			} else {
+				int indexOfExistingItem = playerItems.indexOf(item);
+				playerItems.remove(indexOfExistingItem);
+				playerItems.addFirst(item);
+			}
+		}
+		
+		
+//		for (Integer item : itemsPickedUp) {
+//			if (!playerItems.contains(item)) {
+//				if (playerItems.size() < 3) {
+//					playerItems.add(item);
+//				} else {
+//					droppedItems.add(playerItems.get(playerItems.size()-1));
+//					playerItems.remove(playerItems.size() - 1);
+//					playerItems.add(item);
+//				}
+//			} else {
+//				int indexOfExistingItem = playerItems.indexOf(item);
+//				playerItems.remove(indexOfExistingItem);
+//				droppedItems.add(item);
+//				playerItems.add(item);
+//			}
+//		}
+
+		if (droppedItems.size() > 0) {
+			for (Integer droppedItem : droppedItems) {
+				System.out.println(droppedItem);
+			}
+		} else {
+			System.out.println(0);
+		}
+	}
+}
---
+++ src/nhn_entertainment/WorldOfMatrices.java
@@ -0,0 +1,113 @@
+package nhn_entertainment;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WorldOfMatrices {
+
+	private static Map<Integer, Integer> areaMetadata = new HashMap<>();
+	private static int areaCount = 0;
+
+	private static Map<Integer, List<int[]>> areas = new HashMap<>();
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int size = scanner.nextInt();
+		scanner.nextLine();
+		
+//		System.out.println("enter matrix lines: ");
+		int[][] matrix = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			String row = scanner.nextLine();
+			String[] integerStrings = row.split(" ");
+			for (int k = 0; k < size; k++) {
+				matrix[i][k] = Integer.parseInt(integerStrings[k]);
+//				System.out.print( matrix[i][k]);
+			}
+		}
+		scanner.close();
+
+		for (int i = 0; i < size; i++) {
+			for (int k = 0; k < size; k++) {
+				if (matrix[i][k] == 1) {
+					int existingAreaID = getExistingAreaContainingPoint (i, k);
+//					System.out.println(existingAreaID);
+					if (existingAreaID > -1) {
+//						int areaID = determineExistingArea(i, k);
+						updateAreaSize(existingAreaID, i ,k);
+					} else {
+						foundNewArea(i, k);
+					}
+				}
+			}
+		}
+		
+		List<Integer> areaSizes = new LinkedList<Integer>(areaMetadata.values());
+		
+		if(areaSizes.size() == 0 || areaSizes ==null) {
+			System.out.println(0);
+			return;
+		}
+		
+		Collections.sort(areaSizes); 
+		
+		System.out.println(areaMetadata.size());
+		for(int i=0; i < areaSizes.size(); i++) {
+			System.out.print(areaSizes.get(i));
+			if(i < areaSizes.size()-1) {
+				System.out.print(" ");
+			}
+		}
+		return;
+	}
+
+//	public static boolean isPartOfExistingArea(int row, int column) {
+//		for (Map.Entry<Integer, List<int[]>> area : areas.entrySet()) {
+//			
+//			for(int[] location: area.getValue()) {
+//				if(Math.abs(row - location[0]) + Math.abs(column - location[1]) == 1 ) {
+//					return true;
+//				}
+//			}
+//		}
+//		
+//		return false;
+//	}
+	
+	public static int getExistingAreaContainingPoint(int row, int column) {
+		for (Map.Entry<Integer, List<int[]>> area : areas.entrySet()) {
+			
+			for(int[] location: area.getValue()) {
+				if(Math.abs(row - location[0]) + Math.abs(column - location[1]) == 1 ) {
+					return area.getKey();
+				}
+			}
+		}
+		
+		return -1;
+	}
+
+//	public static int determineExistingArea(int row, int column) {
+//
+//	}
+
+	public static void updateAreaSize(int areaID, int row, int column) {
+//		System.out.println(areaID);
+		areaMetadata.put(areaID, areaMetadata.get(areaID) + 1);
+		int[] newLocation = {row, column};
+		areas.get(areaID).add(newLocation);
+	}
+
+	public static void foundNewArea(int row, int column) {
+		List<int[]> areaLocations = new ArrayList<>();
+		areaLocations.add(new int[]{row, column});
+		areas.put(areaCount, areaLocations);
+
+		areaMetadata.put(areaCount++, 1);
+	}
+}
